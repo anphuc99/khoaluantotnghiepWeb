9d27c08cca23fb2398173cfb5d628bd6c0716a39
anphuc99 <86525411+anphuc99@users.noreply.github.com>
2022-11-26 13:54:52 +0700

    update sql

--- mysql.sql
+++ mysql.sql
@@ -4,15 +4,15 @@ CREATE PROCEDURE GetHistory (
 	IN  playerID INT
 )
 BEGIN
-	SELECT game_gameinfo.gameID, game_gameinfo.playerID, 
-	game_gameinfo.team, game_game.redScore, game_game.blueScore, 
-    game_game.date, 
-    IF((game_gameinfo.team = 0 AND game_game.redScore > game_game.blueScore) OR 
-       (game_gameinfo.team = 1 AND game_game.redScore < game_game.blueScore),0,
-      IF (game_game.redScore = game_game.blueScore,1,2)) as "result"
-    FROM `game_gameinfo` 
-    INNER JOIN `game_game` ON (`game_gameinfo`.`gameID` = `game_game`.`id`)
-    WHERE `game_gameinfo`.`playerID` = playerID
-    ORDER BY `game_game`.`id` DESC
+	SELECT Game_gameinfo.gameID, Game_gameinfo.playerID, 
+	Game_gameinfo.team, Game_game.redScore, Game_game.blueScore, 
+    Game_game.date, 
+    IF((Game_gameinfo.team = 0 AND Game_game.redScore > Game_game.blueScore) OR 
+       (Game_gameinfo.team = 1 AND Game_game.redScore < Game_game.blueScore),0,
+      IF (Game_game.redScore = Game_game.blueScore,1,2)) as "result"
+    FROM `Game_gameinfo` 
+    INNER JOIN `Game_game` ON (`Game_gameinfo`.`gameID` = `Game_game`.`id`)
+    WHERE `Game_gameinfo`.`playerID` = playerID
+    ORDER BY `Game_game`.`id` DESC
     LIMIT 100;
 END$$
